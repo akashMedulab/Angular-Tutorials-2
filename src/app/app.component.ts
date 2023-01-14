fd5ffd5693e739843a7bffcc2f608415bbd46011
akashMedulab <akashmourya@edulab.in>
2023-01-14 18:33:58 +0530

    style binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,15 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title="Nested loop "
- user=['akash','kunal','shivram','ajit']
+  title="style Binding "
+ color='red'
+ bgcolor="blue"
 
- users=[
-  {name:'akash',phone:'37645556',socialAccounts:['facebook','insta','youtube']},
-  {name:'kunal',phone:'37645556',socialAccounts:['youtube','insta','email']},
-  {name:'shivram',phone:'37645556',socialAccounts:['linked','insta','facebook']},
-  {name:'ajit',phone:'37645556',socialAccounts:['email','insta','linked']},
- 
- ]
-   
+ updateColor(){
+
+     this.color="green"
+     this.bgcolor="pink"
+ }
+  
 }
