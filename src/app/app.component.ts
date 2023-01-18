96a13ae30129662be4831d40461f273fec754b8f
akashMedulab <akashmourya@edulab.in>
2023-01-18 12:54:53 +0530

    Basic of ts for anguler

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,24 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title="Basic of ts for angular"
 
-     data:Number=20
+    //  data:Number=20
+    //  getdata(item:number|boolean)
+    //  {
+    //     if(typeof item==="number"){
+
+    //       return item*20
+    //     }
+    //  }//ts define number and boolean value
+
+    data:{name:string,phone:number}={name:'akash',phone:345545}
+
+
+    getdata(item:{name:string,phone:2344})
+      {
+        if(typeof item==="number"){
+
+           return item*20
+         }
+     }//object define in ts
 } 
 
