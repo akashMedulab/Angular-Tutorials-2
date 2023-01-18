7fe2c13d852ea712448f36a065fa8affcbe4e292
akashMedulab <akashmourya@edulab.in>
2023-01-18 19:29:57 +0530

    Template drive form validations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,11 @@ import {FormsModule}from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
-
-
-
 @NgModule({
   declarations: [
     AppComponent,
   
    
-
- 
-
- 
          
   ],
   imports: [
