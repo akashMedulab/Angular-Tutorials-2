c4c1b64be533eec4a223e3aab40defffe04c9e3b
akashMedulab <akashmourya@edulab.in>
2023-01-18 13:30:08 +0530

    Basic of pipes in anguler

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,26 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title="Basic of ts for angular"
+  title="Basic of pipes"
 
-    //  data:Number=20
-    //  getdata(item:number|boolean)
-    //  {
-    //     if(typeof item==="number"){
-
-    //       return item*20
-    //     }
-    //  }//ts define number and boolean value
-
-    data:{name:string,phone:number}={name:'akash',phone:345545}
-
-
-    getdata(item:{name:string,phone:2344})
-      {
-        if(typeof item==="number"){
-
-           return item*20
-         }
-     }//object define in ts
+   today= Date()
 } 
 
