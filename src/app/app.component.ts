7cc649762db17996696a69dfa341b1da28c56a1c
akashMedulab <akashmourya@edulab.in>
2023-01-19 16:29:30 +0530

    Reactive-form-validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, Validators } from '@angular/forms';
 import { FormGroup } from '@angular/forms';
 import { FormControlName } from '@angular/forms';
 
@@ -10,16 +10,29 @@ import { FormControlName } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title="Reactive Form "
+  title="Reactive Form validation"
   loginForm=new  FormGroup ({
-    name:new FormControl(''),
-    email:new FormControl(''),
-    password:new FormControl('')
+    name:new FormControl('',[Validators.required,Validators.pattern('[A-Za-z]+$')]),
+    email:new FormControl('',[Validators.required]),
+    password:new FormControl('',[Validators.required,Validators.minLength(5)])
 
   })
    
   userLogin(){
     console.log(this.loginForm.value)
   }
+
+  get name()
+  {
+     return this.loginForm.get('name')
+  }
+
+  get email(){
+    return this.loginForm.get('email')
+  }
+  get password(){
+    return this.loginForm.get('password')
+  }
 } 
+
 
